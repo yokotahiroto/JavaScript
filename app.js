f833810b053eedd5bdabb221f205044a7519b271
横田紘人 <yokotahiroto@yokotahiroshininnoMacBook-Pro.local>
2020-12-08 20:28:53 +0900

    [start]3章

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-alert('Hello World');
+var orange = 100;
+var apple = 120;
+
+if(orange < apple){
+  alert('みかんの値段がりんごより安い');
+} else if(orange == apple){
+  alert('みかんとりんごが同じ値段')
+} else{
+  alert('みかんの値段がりんごより高い')；
+}
+
+var alertString;
+alertString = addString("WebCamp");
+
+alert(alertString);
+
+function addString(strA){
+  var addStr = "Hello " + strA;
+  return addStr;
+}
